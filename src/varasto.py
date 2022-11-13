fa32f01f060a6ae0e58d260c8b41a28a945209ec
Lotta Hyppyrä <lotta@hyppyra.net>
2022-11-13 21:33:15 +0200

    Pylint ja koodin staattinen analyysi

--- src/varasto.py
+++ src/varasto.py
@@ -1,16 +1,12 @@
-"""Luokka varasto."""
 class Varasto:
-    """Luokka varasto, jolla tilavuus ja saldo."""
     def __init__(self, tilavuus, alku_saldo = 0) -> None:
-        """Konstruktorissa määritetään varaston tilavuus ja saldo."""
-        self.tilavuus = max(0.0, tilavuus)
-        self.saldo = min(max(0.0, alku_saldo), tilavuus)
+        self.tilavuus = int(max(0.0, tilavuus))
+        self.saldo = int(min(max(0.0, alku_saldo), tilavuus))
+
     def paljonko_mahtuu(self):
-        """Palauttaa paljonko varastoon vielä mahtuu."""
         return self.tilavuus - self.saldo
 
     def lisaa_varastoon(self, maara):
-        """Lisää parametrin verran varastoon, jos mahtuu."""
         if maara < 0:
             return
         if maara <= self.paljonko_mahtuu():
@@ -19,7 +15,6 @@ class Varasto:
             self.saldo = self.tilavuus
 
     def ota_varastosta(self, maara):
-        """Ottaa parametrin verran pois tilavuudesta, jos mahdollista."""
         if maara < 0:
             return 0.0
         if maara > self.saldo:
@@ -32,6 +27,5 @@ class Varasto:
         return maara
 
     def __str__(self):
-        """Kertoo varaston saldon ja paljon tilaa vielä jäljellä"""
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
     
